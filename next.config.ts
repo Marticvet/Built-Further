7f69b523bbd6f8cece7ec378aae3f6ece2a7d8ca
Martin Tsvetanov <martigiant@gmail.com>
2026-08-24 17:55:53 +0200

    PostHog (might be fix)

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,8 @@
 import type { NextConfig } from "next";
 
-const posthogProxyPath = "/bfx";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
   skipTrailingSlashRedirect: true,
-  async rewrites() {
-    return [
-      {
-        source: `${posthogProxyPath}/static/:path*`,
-        destination: "https://eu-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: `${posthogProxyPath}/array/:path*`,
-        destination: "https://eu-assets.i.posthog.com/array/:path*",
-      },
-      {
-        source: `${posthogProxyPath}/:path*`,
-        destination: "https://eu.i.posthog.com/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
